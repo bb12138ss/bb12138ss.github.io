7688435c23b204021bd1112c4272e808db51dd49
bb12138ss <1440985392@qq.com>
2023-03-14 16:55:42 +0800

    Site updated: 2023-03-14 16:55:38

---
+++ js/mouse.js
@@ -0,0 +1,39 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#FF0000"
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            3000,
+            function() {
+                $i.remove();
+            });
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
+
+
+
+/*作者: 橘子
+链接: https: //lavender-z.github.io/2020/12/28/Blog/58148/
+    来源: 橘子的小站
+著作权归作者所有。 商业转载请联系作者获得授权， 非商业转载请注明出处。*/</span>
