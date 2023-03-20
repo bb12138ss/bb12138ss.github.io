96eea1701f85f342885d8c3720737a9ec0f2ef33
bb12138ss <1440985392@qq.com>
2023-03-20 08:43:18 +0800

    Site updated: 2023-03-20 08:43:14

--- js/txmap.js
+++ js/txmap.js
@@ -219,8 +219,8 @@ function showWelcome() {
             `<b><center>你好鸭~</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}<b>得闲饮茶</b>`;
     } catch (err) {
         // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
-    }
+    };
 }
 window.onload = showWelcome;
 // 如果使用了pjax在加上下面这行代码
-document.addEventListener('pjax:complete', showWelcome);</b>
+// document.addEventListener('pjax:complete', showWelcome);</b>
